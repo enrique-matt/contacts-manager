f71a0cc5004421bb3dc9abc4c6b34adc420eac34
MattBrozusky <matthew.brozusky@gmail.com>
2018-11-27 11:06:58 -0600

    made show format better

--- src/contactsFunctionality/ShowContacts.java
+++ src/contactsFunctionality/ShowContacts.java
@@ -8,19 +8,15 @@ public class ShowContacts {
 
     protected static void showContact(List<String> contactsListString){
 
-        System.out.format("%nName | Phone Number%n----------------------%n");
+        System.out.format("%nName            | Phone Number   |%n----------------------------------%n");
 
+        for (String contact : contactsListString) {
 
-        for (String contact : contactsListString){
+            int separatorNum = contact.indexOf("|");
+            String nameOnly = contact.substring(0, separatorNum - 1);
+            String numberOnly = contact.substring(separatorNum);
 
-            System.out.println(contact);
-
+            System.out.format("%-14s  %-12s |%n", nameOnly, numberOnly);
         }
-
     }
-//    Name | Phone number
-//---------------
-//    Jack Blank | 1231231234
-//    Jane Doe | 2342342345
-//    Sam Space | 3453453456
 }
